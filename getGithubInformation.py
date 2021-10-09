b8a6983ab9eb3b1ba3862df3ee95d0bfe851d179
ZhangXD <1296753187@qq.com>
2021-10-09 15:50:46 +0800

    GitHub的api调用

---
+++ getGithubInformation.py
@@ -0,0 +1,38 @@
+
+# 更改url为GitHub的api
+import json
+
+import requests
+
+from MyDataBase import MyDataBase
+
+
+def changeUrl(url):
+    strs = url.split("/")
+    l = len(strs)
+    res = strs[0] + "//" + "api." + strs[2] + "/repos/"
+    for i in range(3, l):
+        res = res + strs[i] + "/"
+    return res
+
+
+myDataBase = MyDataBase()
+dataBaseSize = myDataBase.getSize()
+
+for i in range(1, dataBaseSize+1):
+    print("="*50)
+    githubUrl = myDataBase.getElementBySerial("github_url", i)
+    if githubUrl is None or len(githubUrl) <= 0:
+        print("序列{0}无GitHub项目".format(i))
+    else:
+        githubApi = changeUrl(githubUrl)
+        r = requests.get(githubApi)
+        d = json.loads(r.text)
+        if d.get('name') is None or d.get('watchers') is None:
+            print("序号{0}无法查询到详细信息,项目地址为{1}".format(i, githubUrl))
+        else:
+            name = d['name']
+            star = d['watchers']
+            print("序号{0}的GitHub链接为{1},其项目名称为{2},项目star为{3}".format(i, githubUrl, name, star))
+            myDataBase.updateElementBySerial("project_name", name, i)
+            myDataBase.updateElementBySerial("star", star, i)
--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-import requests
+import json
 
-r = requests.get("https://www.freebuf.com/sectool/287344.html")
-print(r.text)
+import requests
+url = "https://github.com/boy-hack"
+r = requests.get("https://api.github.com/repos/MichaelGrafnetter/DSInternals")
+d = json.loads(r.text)
+
+def changeUrl(url):
+    strs = url.split("/")
+    l = len(strs)
+    res = strs[0] + "//" + "api." + strs[2] + "/repos/"
+    for i in range(3, l):
+        res = res + strs[i] + "/"
+    return res
+
+print(changeUrl(url))
+print(d.get('name'))
