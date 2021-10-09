847c6606b49a9c31e0579de8ed000397e3ba87b3
ZhangXD <1296753187@qq.com>
2021-10-09 11:06:45 +0800

    多线程和单线程分开，请求头改为调用模块执行

--- getLinks.py
+++ getLinks.py
@@ -18,7 +18,6 @@ def getResponose(url):
     print(header)
     try:
         response = requests.get(url, headers=header)
-        time.sleep(1)
     except:
         print("{0}访问失败".format(url))
     return response
@@ -46,5 +45,22 @@ def getInformation(start, end):
             myDataBase.updateElementBySerial("github_url", github_url, i)
 
 
+# 设置最大线程数
+POOL_SIZE = 10
+pool = ThreadPoolExecutor(max_workers=POOL_SIZE)
+# 根据线程数切分任务
 dataBaseSize = myDataBase.getSize()
-getInformation(1, dataBaseSize+1)
+k = int(dataBaseSize / POOL_SIZE)
+start = 1
+for i in range(1, POOL_SIZE):
+    pool.submit(getInformation, start, start+k)
+    s = "******线程{0}正在执行[{1}, {2}]任务段******".format(i, start, start+k)
+    print("*" * (len(s) + 5))
+    print(s)
+    print("*" * (len(s) + 5))
+    start += k
+pool.submit(start, dataBaseSize+1)
+fs = "******线程{0}正在执行[{1}, {2}]任务段******".format(POOL_SIZE, start, dataBaseSize)
+print("*" * (len(fs) + 5))
+print(s)
+print("*" * (len(fs) + 5))
--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-# 设置最大线程数
-from concurrent.futures import ThreadPoolExecutor
-from MyDataBase import MyDataBase
+import requests
 
-
-def printNum(k, j):
-    print(k)
-    print(j)
-
-
-POOL_SIZE = 10
-pool = ThreadPoolExecutor(max_workers=POOL_SIZE)
-
-z = 0
-for i in range(1, POOL_SIZE):
-    pool.submit(printNum, z, z+1)
-    s = "******线程{0}正在执行******".format(i)
-    print("*" * (len(s) + 5))
-    print(s)
-    print("*" * (len(s) + 5))
+r = requests.get("https://www.freebuf.com/sectool/287344.html")
+print(r.text)
